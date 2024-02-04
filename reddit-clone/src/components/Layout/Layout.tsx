6167db2ce7056cf2ce67fdc973e2278e0436502b
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-04 17:47:00 +0000

    Installed Chakra & next and set up bare-bones files

---
+++ reddit-clone/src/components/Layout/Layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Layout:React.FC = () => {
+
+    return <div>Have a good coding</div>
+}
+export default Layout;
---
+++ reddit-clone/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import type { AppProps } from "next/app" 
+
+
+export default function Home() {
+  return (
+    <div>Hello</div>
+  )
+}
