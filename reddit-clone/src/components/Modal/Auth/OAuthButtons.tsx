5b0a5f3fbd0d4929bf83dc51691c926d13643d99
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-05 12:50:27 +0000

    Added Sign up and Login as well as raw OAuthButtons

---
+++ reddit-clone/src/components/Modal/Auth/OAuthButtons.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+
+const OAuthButtons:React.FC = () => {
+    
+    return (
+       <Flex>
+            <Button>Continue with Google</Button>
+       </Flex> 
+    );
+};
+export default OAuthButtons;
