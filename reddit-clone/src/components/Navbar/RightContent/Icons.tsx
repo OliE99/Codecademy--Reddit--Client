d8f114ef7947703143c1f1553fc8acb0c3ef3b7d
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-12 10:51:45 +0000

    Added and finished Icons on the Navbar

--- reddit-clone/src/components/Navbar/RightContent/Icons.tsx
+++ reddit-clone/src/components/Navbar/RightContent/Icons.tsx
@@ -9,10 +9,16 @@ import {
   IoVideocamOutline,
 } from "react-icons/io5";
 
-const Icons: React.FC = () => {
-    return (
-        <Flex>
-            <Flex
+const ActionIcons: React.FC = () => {
+  return (
+    <Flex alignItems="center" flexGrow={1}>
+      <Box
+        display={{ base: "none", md: "flex" }}
+        alignItems="center"
+        borderRight="1px solid"
+        borderColor="gray.200"
+      >
+        <Flex
           mr={1.5}
           ml={1.5}
           padding={1}
@@ -42,8 +48,42 @@ const Icons: React.FC = () => {
         >
           <Icon as={IoVideocamOutline} fontSize={22} />
         </Flex>
-        
-            <></>
+      </Box>
+      <>
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={BsChatDots} fontSize={20} />
         </Flex>
-    );
+        <Flex
+          mr={1.5}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={IoNotificationsOutline} fontSize={20} />
+        </Flex>
+        <Flex
+          display={{ base: "none", md: "flex" }}
+          mr={3}
+          ml={1.5}
+          padding={1}
+          cursor="pointer"
+          borderRadius={4}
+          _hover={{ bg: "gray.200" }}
+        >
+          <Icon as={GrAdd} fontSize={20} />
+        </Flex>
+      </>
+    </Flex>
+  );
 };
+
+export default ActionIcons;
