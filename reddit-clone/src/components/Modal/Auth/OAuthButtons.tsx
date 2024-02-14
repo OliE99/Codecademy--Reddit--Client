9646430f03b08e74cd358ad594e53db9675e948e
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-14 16:23:42 +0000

    Update OAuthButtons.tsx

--- reddit-clone/src/components/Modal/Auth/OAuthButtons.tsx
+++ reddit-clone/src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,30 +1,42 @@
-import { Button, Flex, Image, Text } from "@chakra-ui/react";
-import React from "react";
-import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { auth } from "../../../firebase/clientApp";
+import { Button, Flex, Image, Text, useColorModeValue } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import { collection, doc, setDoc } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../firebase/clientApp";
 
-type OAuthButtonsProps = {};
+const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, userCred, loading, error] =
+    useSignInWithGoogle(auth);
+  const hoverBg = useColorModeValue("gray.50", "#2A4365");
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, "users", user.uid);
 
-const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
-  const [signInWithGoogle, _, loading, error] = useSignInWithGoogle(auth);
+    await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+  };
+
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
 
   return (
-    <Flex direction="column" mb={4} width="100%">
+    <Flex direction="column" width="100%" mb={4}>
       <Button
         variant="oauth"
+        _hover={{ bg: hoverBg }}
         mb={2}
-        onClick={() => signInWithGoogle()}
         isLoading={loading}
+        onClick={() => signInWithGoogle()}
       >
         <Image src="/images/googlelogo.png" height="20px" mr={4} />
         Continue with Google
       </Button>
-      <Button variant="oauth">Some Other Provider</Button>
-      {error && (
-        <Text textAlign="center" fontSize="10pt" color="red" mt={2}>
-          {error}
-        </Text>
-      )}
+      <Button variant="oauth" _hover={{ bg: hoverBg }}>
+        Some Other Provider
+      </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
