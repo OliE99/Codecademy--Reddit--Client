5fa056422c149c44ad001f1d4d02b3240d12256f
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-14 16:27:31 +0000

    Update index.tsx

--- reddit-clone/src/components/Modal/CreateCommunity/index.tsx
+++ reddit-clone/src/components/Modal/CreateCommunity/index.tsx
@@ -1,231 +1,271 @@
-import React, { useState } from "react";
 import {
   Box,
   Button,
   Checkbox,
-  Divider,
   Flex,
   Icon,
   Input,
+  Modal,
   ModalBody,
   ModalCloseButton,
+  ModalContent,
   ModalFooter,
   ModalHeader,
+  ModalOverlay,
   Stack,
   Text,
+  useColorModeValue,
 } from "@chakra-ui/react";
-import { doc, runTransaction, serverTimestamp } from "firebase/firestore";
+import {
+  doc,
+  runTransaction,
+  serverTimestamp,
+  Timestamp,
+} from "firebase/firestore";
 import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { BsFillEyeFill, BsFillPersonFill } from "react-icons/bs";
 import { HiLockClosed } from "react-icons/hi";
-import { useSetRecoilState } from "recoil";
-import { communityState } from "../../../atoms/communitiesAtom";
-import { firestore } from "../../../firebase/clientApp";
-import ModalWrapper from "../ModalWrapper";
 
-type CreateCommunityModalProps = {
-  isOpen: boolean;
+import { auth, firestore } from "../../../firebase/clientApp";
+import useDirectory from "../../../hooks/useDirectory";
+
+type CreateCommunityModelProps = {
+  open: boolean;
   handleClose: () => void;
-  userId: string;
 };
 
-const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
-  isOpen,
+const CreateCommunityModel: React.FC<CreateCommunityModelProps> = ({
+  open,
   handleClose,
-  userId,
 }) => {
-  const setSnippetState = useSetRecoilState(communityState);
-  const [name, setName] = useState("");
+  const [user] = useAuthState(auth);
+  const [CommunitiesName, setCommunities] = useState("");
   const [charsRemaining, setCharsRemaining] = useState(21);
-  const [nameError, setNameError] = useState("");
-  const [communityType, setCommunityType] = useState("public");
+  const [communityType, setCommunityType] = useState("");
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const { toggleMenuOpen } = useDirectory();
+  const bg = useColorModeValue("gray.100", "#1A202C");
+  const textColor = useColorModeValue("gray.500", "gray.400");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.value.length > 21) return;
-    setName(event.target.value);
+
+    setCommunities(event.target.value);
     setCharsRemaining(21 - event.target.value.length);
-  };
-
-  const handleCreateCommunity = async () => {
-    if (nameError) setNameError("");
-    const format = /[ `!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
-
-    if (format.test(name) || name.length < 3) {
-      return setNameError(
-        "Community names must be between 3–21 characters, and can only contain letters, numbers, or underscores."
-      );
-    }
-
-    setLoading(true);
-    try {
-      // Create community document and communitySnippet subcollection document on user
-      const communityDocRef = doc(firestore, "communities", name);
-      await runTransaction(firestore, async (transaction) => {
-        const communityDoc = await transaction.get(communityDocRef);
-        if (communityDoc.exists()) {
-          throw new Error(`Sorry, /r${name} is taken. Try another.`);
-        }
-
-        transaction.set(communityDocRef, {
-          creatorId: userId,
-          createdAt: serverTimestamp(),
-          numberOfMembers: 1,
-          privacyType: "public",
-        });
-
-        transaction.set(
-          doc(firestore, `users/${userId}/communitySnippets`, name),
-          {
-            communityId: name,
-            isModerator: true,
-          }
-        );
-      });
-    } catch (error: any) {
-      console.log("Transaction error", error);
-      setNameError(error.message);
-    }
-    setSnippetState((prev) => ({
-      ...prev,
-      mySnippets: [],
-    }));
-    handleClose();
-    router.push(`r/${name}`);
-    setLoading(false);
   };
 
   const onCommunityTypeChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const {
-      target: { name },
-    } = event;
-    if (name === communityType) return;
-    setCommunityType(name);
+    setCommunityType(event.target.name);
+  };
+
+  const handleCreateCommunity = async () => {
+    if (error) setError("");
+
+    const format = /[ `!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if (format.test(CommunitiesName) || CommunitiesName.length < 3) {
+      return setError(
+        "Community names must be between 3–21 characters, and can only contain letters, numbers, or underscores."
+      );
+    }
+
+    setLoading(true);
+
+    try {
+      const communityDocRef = doc(firestore, "communities", CommunitiesName);
+
+      await runTransaction(firestore, async (transaction) => {
+        const communityDoc = await transaction.get(communityDocRef);
+        if (communityDoc.exists()) {
+          throw new Error(
+            `Sorry, r/${CommunitiesName} is MdTakeoutDining. Try Another`
+          );
+          return;
+        }
+
+        await transaction.set(communityDocRef, {
+          creatorId: user?.uid,
+          createdAt: serverTimestamp(),
+          numberOfMembers: 1,
+          privacyTYpe: communityType,
+        });
+
+        //update
+        updateCommunitySnippet(user?.uid!, transaction);
+
+        // create
+        transaction.set(
+          doc(
+            firestore,
+            `users/${user?.uid}/communitySnippets`,
+            CommunitiesName
+          ),
+          {
+            communityId: CommunitiesName,
+            isModerator: true,
+            updateTimeStamp: serverTimestamp() as Timestamp,
+          }
+        );
+      });
+
+      handleClose();
+      toggleMenuOpen();
+      setCommunityType("");
+      setCommunities("");
+      router.push(`r/${CommunitiesName}`);
+    } catch (error: any) {
+      console.log("HandleCreateCommunity Error", error);
+      setError(error.message);
+    }
+
+    setLoading(false);
+    //setError("")
+  };
+
+  const updateCommunitySnippet = async (userId: string, transaction: any) => {
+    if (!userId) return;
+
+    const communityUpdateDocRef = doc(
+      firestore,
+      `communities/${CommunitiesName}/userInCommunity/${userId}`
+    );
+
+    await transaction.set(communityUpdateDocRef, {
+      userId: userId,
+      userEmail: user?.email,
+    });
   };
 
   return (
-    <ModalWrapper isOpen={isOpen} onClose={handleClose}>
-      <ModalHeader
-        display="flex"
-        flexDirection="column"
-        fontSize={15}
-        padding={3}
-      >
-        Create a community
-      </ModalHeader>
-      <Box pr={3} pl={3}>
-        <Divider />
-        <ModalCloseButton />
-        <ModalBody display="flex" flexDirection="column" padding="10px 0px">
-          <Text fontWeight={600} fontSize={15}>
-            Name
-          </Text>
-          <Text fontSize={11} color="gray.500">
-            Community names including capitalization cannot be changed
-          </Text>
-          <Text
-            color="gray.400"
-            position="relative"
-            top="28px"
-            left="10px"
-            width="20px"
+    <>
+      <Modal isOpen={open} onClose={handleClose} size="lg">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            display="flex"
+            flexDirection="column"
+            fontSize={15}
+            padding={3}
           >
-            r/
-          </Text>
-          <Input
-            position="relative"
-            name="name"
-            value={name}
-            onChange={handleChange}
-            pl="22px"
-            type={""}
-            size="sm"
-          />
-          <Text
-            fontSize="9pt"
-            color={charsRemaining === 0 ? "red" : "gray.500"}
-            pt={2}
-          >
-            {charsRemaining} Characters remaining
-          </Text>
-          <Text fontSize="9pt" color="red" pt={1}>
-            {nameError}
-          </Text>
-          <Box mt={4} mb={4}>
-            <Text fontWeight={600} fontSize={15}>
-              Community Type
-            </Text>
-            <Stack spacing={2} pt={1}>
-              <Checkbox
-                colorScheme="blue"
-                name="public"
-                isChecked={communityType === "public"}
-                onChange={onCommunityTypeChange}
+            Create a Community
+          </ModalHeader>
+
+          <Box pl={3} pr={3}>
+            <ModalCloseButton />
+            <ModalBody display="flex" flexDirection="column" padding="10px 0px">
+              <Text fontWeight={600} fontSize={15}>
+                Name
+              </Text>
+              <Text fontSize={11} color={textColor}>
+                Community Names including capitalization cannot be changed
+              </Text>
+              <Text
+                position="relative"
+                top="28px"
+                left="10px"
+                width="20px"
+                color="gray.400"
               >
-                <Flex alignItems="center">
-                  <Icon as={BsFillPersonFill} mr={2} color="gray.500" />
-                  <Text fontSize="10pt" mr={1}>
-                    Public
-                  </Text>
-                  <Text fontSize="8pt" color="gray.500" pt={1}>
-                    Anyone can view, post, and comment to this community
-                  </Text>
-                </Flex>
-              </Checkbox>
-              <Checkbox
-                colorScheme="blue"
-                name="restricted"
-                isChecked={communityType === "restricted"}
-                onChange={onCommunityTypeChange}
+                r/
+              </Text>
+              <Input
+                position="relative"
+                value={CommunitiesName}
+                size="sm"
+                pl="22px"
+                onChange={handleChange}
+              />
+              <Text
+                fontSize="9pt"
+                color={charsRemaining === 0 ? "red" : textColor}
               >
-                <Flex alignItems="center">
-                  <Icon as={BsFillEyeFill} color="gray.500" mr={2} />
-                  <Text fontSize="10pt" mr={1}>
-                    Restricted
-                  </Text>
-                  <Text fontSize="8pt" color="gray.500" pt={1}>
-                    Anyone can view this community, but only approved users can
-                    post
-                  </Text>
-                </Flex>
-              </Checkbox>
-              <Checkbox
-                colorScheme="blue"
-                name="private"
-                isChecked={communityType === "private"}
-                onChange={onCommunityTypeChange}
-              >
-                <Flex alignItems="center">
-                  <Icon as={HiLockClosed} color="gray.500" mr={2} />
-                  <Text fontSize="10pt" mr={1}>
-                    Private
-                  </Text>
-                  <Text fontSize="8pt" color="gray.500" pt={1}>
-                    Only approved users can view and submit to this community
-                  </Text>
-                </Flex>
-              </Checkbox>
-            </Stack>
+                {charsRemaining} Characters Remaining
+              </Text>
+              <Text fontSize="9pt" color="red" pt={1}>
+                {error}
+              </Text>
+              <Box mt={4} mb={4}>
+                <Text fontWeight={600} fontSize={15}>
+                  Community Type
+                </Text>
+                <Stack spacing={2}>
+                  <Checkbox
+                    name="public"
+                    isChecked={communityType === "public"}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align="center">
+                      <Icon as={BsFillPersonFill} color={textColor} mr={2} />
+                      <Text fontSize="10pt" mr={1}>
+                        Public
+                      </Text>
+                      <Text fontSize="8pt" color={textColor} pt={1}>
+                        Anyone can view, post and comment to this community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name="restricted"
+                    isChecked={communityType === "restricted"}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align="center">
+                      <Icon as={BsFillEyeFill} color={textColor} mr={2} />
+                      <Text fontSize="10pt" mr={1}>
+                        Restricted
+                      </Text>
+                      <Text fontSize="8pt" color={textColor} pt={1}>
+                        Anyone can view this community, but only approved users
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                  <Checkbox
+                    name="private"
+                    isChecked={communityType === "private"}
+                    onChange={onCommunityTypeChange}
+                  >
+                    <Flex align="center">
+                      <Icon as={HiLockClosed} color={textColor} mr={2} />
+                      <Text fontSize="10pt" mr={1}>
+                        Private
+                      </Text>
+                      <Text fontSize="8pt" color={textColor} pt={1}>
+                        Only approved users can view and submit to this
+                        community
+                      </Text>
+                    </Flex>
+                  </Checkbox>
+                </Stack>
+              </Box>
+            </ModalBody>
           </Box>
-        </ModalBody>
-      </Box>
-      <ModalFooter bg="gray.100" borderRadius="0px 0px 10px 10px">
-        <Button variant="outline" height="30px" mr={2} onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button
-          variant="solid"
-          height="30px"
-          onClick={handleCreateCommunity}
-          isLoading={loading}
-        >
-          Create Community
-        </Button>
-      </ModalFooter>
-    </ModalWrapper>
+
+          <ModalFooter bg={bg} borderRadius="0px 0px 10px 10px">
+            <Button
+              variant="outline"
+              height="30px"
+              mr={3}
+              onClick={handleClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              height="30px"
+              onClick={handleCreateCommunity}
+              isLoading={loading}
+            >
+              Create Community
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
-export default CreateCommunityModal;
+export default CreateCommunityModel;
