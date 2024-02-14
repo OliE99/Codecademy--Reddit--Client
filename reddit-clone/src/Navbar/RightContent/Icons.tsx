ba456c4a3fa163bd42cf26ed4e734119d998b84e
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-14 16:02:48 +0000

    Update to Icons code

--- reddit-clone/src/Navbar/RightContent/Icons.tsx
+++ reddit-clone/src/Navbar/RightContent/Icons.tsx
@@ -1,6 +1,13 @@
+import {
+  Flex,
+  Icon,
+  keyframes,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 import React from "react";
-import { AddIcon } from "@chakra-ui/icons";
-import { Box, Flex, Icon } from "@chakra-ui/react";
 import { BsArrowUpRightCircle, BsChatDots } from "react-icons/bs";
 import { GrAdd } from "react-icons/gr";
 import {
@@ -8,18 +15,27 @@ import {
   IoNotificationsOutline,
   IoVideocamOutline,
 } from "react-icons/io5";
-import useDirectory from "../../hooks/useDirectory";
 
-type ActionIconsProps = {};
+const animationKeyframes = keyframes`
+  0% { transform: scale(1) rotate(0); border-radius: 20%; }
+  25% { transform: scale(2) rotate(0); border-radius: 20%; }
+  50% { transform: scale(2) rotate(270deg); border-radius: 50%; }
+  75% { transform: scale(1) rotate(270deg); border-radius: 50%; }
+  100% { transform: scale(1) rotate(0); border-radius: 20%; }
+`;
 
-const ActionIcons: React.FC<ActionIconsProps> = () => {
-  const { toggleMenuOpen } = useDirectory();
+const animation = `${animationKeyframes} 2s ease-in-out infinite`;
+
+const Icons: React.FC = () => {
+  const router = useRouter();
+  const hoverBg = useColorModeValue("gray.200", "#2A4365");
+
   return (
-    <Flex alignItems="center" flexGrow={1}>
-      <Box
+    <Flex>
+      <Flex
         display={{ base: "none", md: "flex" }}
-        alignItems="center"
-        borderRight="1px solid"
+        align="center"
+        borderRadius="1px solid"
         borderColor="gray.200"
       >
         <Flex
@@ -28,7 +44,7 @@ const ActionIcons: React.FC<ActionIconsProps> = () => {
           padding={1}
           cursor="pointer"
           borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+          _hover={{ bg: hoverBg }}
         >
           <Icon as={BsArrowUpRightCircle} fontSize={20} />
         </Flex>
@@ -38,7 +54,7 @@ const ActionIcons: React.FC<ActionIconsProps> = () => {
           padding={1}
           cursor="pointer"
           borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+          _hover={{ bg: hoverBg }}
         >
           <Icon as={IoFilterCircleOutline} fontSize={22} />
         </Flex>
@@ -48,11 +64,11 @@ const ActionIcons: React.FC<ActionIconsProps> = () => {
           padding={1}
           cursor="pointer"
           borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+          _hover={{ bg: hoverBg }}
         >
           <Icon as={IoVideocamOutline} fontSize={22} />
         </Flex>
-      </Box>
+      </Flex>
       <>
         <Flex
           mr={1.5}
@@ -60,8 +76,21 @@ const ActionIcons: React.FC<ActionIconsProps> = () => {
           padding={1}
           cursor="pointer"
           borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+          _hover={{ bg: hoverBg }}
+          onClick={() => router.push("/chat")}
+          position="relative"
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
         >
+          <Text
+            position="absolute"
+            top={-1}
+            left={4}
+            fontSize={10}
+            as={motion.div}
+            animation={animation}
+          >
+            New
+          </Text>
           <Icon as={BsChatDots} fontSize={20} />
         </Flex>
         <Flex
@@ -70,18 +99,18 @@ const ActionIcons: React.FC<ActionIconsProps> = () => {
           padding={1}
           cursor="pointer"
           borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+          _hover={{ bg: hoverBg }}
         >
           <Icon as={IoNotificationsOutline} fontSize={20} />
         </Flex>
         <Flex
           display={{ base: "none", md: "flex" }}
-          mr={3}
+          mr={1.5}
           ml={1.5}
           padding={1}
           cursor="pointer"
           borderRadius={4}
-          _hover={{ bg: "gray.200" }}
+          _hover={{ bg: hoverBg }}
         >
           <Icon as={GrAdd} fontSize={20} />
         </Flex>
@@ -89,5 +118,4 @@ const ActionIcons: React.FC<ActionIconsProps> = () => {
     </Flex>
   );
 };
-
-export default ActionIcons;
+export default Icons;
