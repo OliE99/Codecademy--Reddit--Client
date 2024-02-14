d686050c6903011df9ab927f4cd7dc3442e9f2bd
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-14 16:21:36 +0000

    Tided code and replaced some files

--- reddit-clone/src/components/Layout/PageContent.tsx
+++ reddit-clone/src/components/Layout/PageContent.tsx
@@ -1,18 +1,23 @@
+import { Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
 
-interface PageContentLayoutProps {
-  maxWidth?: string;
-}
+type PageContentProps = {
+  children: any;
+};
 
-// Assumes array of two children are passed
-const PageContentLayout: React.FC<PageContentLayoutProps> = ({
-  children,
-  maxWidth,
-}) => {
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  const router = useRouter();
+  const uid = router.query;
+
   return (
     <Flex justify="center" p="16px 0px">
-      <Flex width="95%" justify="center" maxWidth={maxWidth || "860px"}>
+      <Flex
+        width="95%"
+        justify="center"
+        maxWidth={uid.uid ? "1160px" : "860px"}
+      >
+        {/* Left */}
         <Flex
           direction="column"
           width={{ base: "100%", md: "65%" }}
@@ -20,17 +25,17 @@ const PageContentLayout: React.FC<PageContentLayoutProps> = ({
         >
           {children && children[0 as keyof typeof children]}
         </Flex>
-        {/* Right Content */}
-        <Box
+
+        {/* Right */}
+        <Flex
+          direction="column"
           display={{ base: "none", md: "flex" }}
-          flexDirection="column"
           flexGrow={1}
         >
           {children && children[1 as keyof typeof children]}
-        </Box>
+        </Flex>
       </Flex>
     </Flex>
   );
 };
-
-export default PageContentLayout;
+export default PageContent;
--- reddit-clone/src/components/Layout/index.tsx
+++ reddit-clone/src/components/Layout/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase/clientApp";
-import useAuth from "../../hooks/useAuth";
-import Navbar from "../../Navbar";
-import AuthModal from "../Modal/Auth";
+import Navbar from "../../Navbar/Navbar";
 
-const Layout: React.FC = ({ children }) => {
-  // useAuth(); // will implement later at end of tutorial
+type LayoutProps = {
+  children: any;
+};
 
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Navbar />
-      {children}
+      <main>{children}</main>
     </>
   );
 };
